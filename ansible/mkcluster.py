dd52e6a2cde480a02151860f3a3d47eb04258476
Dexter Taylor <binarymachineshop@gmail.com>
2018-09-08 19:32:03 -0700

    cluster config script in progress

--- ansible/mkcluster.py
+++ ansible/mkcluster.py
@@ -9,7 +9,17 @@ from snap import cli_tools as cli
 from cli import mandatory_input
 
 
-CouchbaseBucketSpec = namedtuple('CouchbaseBucketSpec', 'name ram_quota num_replicas')
+CouchbaseBucketSpec = namedtuple('CouchbaseBucketSpec', 'name type ram_quota num_replicas')
+
+BUCKET_TYPE_OPTIONS = [
+	{'label': 'couchbase', 'value': 'couchbase'},
+	{'label': 'memcache', 'value': 'memcache'}
+]
+	
+
+class MissingInput(Exception):
+    def __init__(self, message):
+        Exception.__init__(self, message)
 
 
 
@@ -17,6 +27,7 @@ class MakeClusterCLI(Cmd):
 	def __init__(self, app_name='mkcluster', **kwargs):
 		Cmd.__init__(self)
 		self.prompt = '%s [%s] > ' % (self.name, self.project_name)
+		self.cluster_config = {}
 		_ = os.system('clear')
 
 
@@ -29,7 +40,35 @@ class MakeClusterCLI(Cmd):
 
 
 	def create_bucket(self):
-		with mandatory_input()
+		bucket_name = None
+		quota = None
+		num_replicas = None
+
+		with mandatory_input(cli.InputPrompt('bucket name'),						
+							 warning_message='a bucket must have a name',
+							 failure_message='bad or missing bucket name') as input_result:
+			bucket_name = input_result.data
+
+		bucket_type = cli.MenuPrompt('bucket type', BUCKET_TYPE_OPTIONS).show()
+
+		with required_input_format(INT_REGEX,
+								   cli.InputPrompt('bucket RAM quota'),
+								   warning_message='RAM quota must be a positive integer',
+								   failure_message='bad RAM quota value') as input_result:
+			quota = input_result.data
+		
+		value_test = lambda x: x < 5
+		with constrained_input_value(value_test,
+									 cli.InputPrompt('number of replicas'),
+									 warning_message='# of replicas must be less than 5',
+									 failure_message='# of replicas outside of allowed limits') as input_result:
+			num_replicas = input_result.data									
+
+		return CouchbaseBucketSpec(name=bucket_name,
+								   type=bucket_type,
+								   ram_quota=quota,
+								   num_replicas=num_replicas)
+		
 
 	def do_new(self, cmd_args):
 		admin_username = self.get_admin_username()
@@ -41,19 +80,45 @@ class MakeClusterCLI(Cmd):
 			bucket_spec = self.create_bucket()
 			if bucket_spec:
 				buckets.append(bucket_spec)
+				print('added bucket specification')
 			else:
 				print('No bucket created.')
-			if not len(buckets):
-				print('A working couchbase cluster must have at least one bucket.')
-				print('')
+			if len(buckets):
+				should_continue = cli.InputPrompt('Create another bucket (Y/n)?', 'y').show()
+				if should_continue == 'n':
+					break
 
+			else:
+				print('A working couchbase cluster must have at least one bucket.')
+				should_create = cli.InputPrompt('Create bucket (Y/n?', 'y').show()
+				if should_create == 'n':
+					break
+			
+
+		if not len(buckets):
+			print('no cluster config created.')
+		else:
+			self.cluster_config = {
+				'admin_username': admin_username,
+				'admin_password': admin_password,
+				'cluster_ram_quota': cluster_ram_quota,
+				'buckets': buckets 
+			}
+
+			print('created cluster config:')
+			print(common.jsonpretty(self.cluster_config))
+
+
+		
 	def do_save(self, cmd_args):
 		pass
 
 
 def main():
-	
-
+	cli_app = MakeClusterCLI()
+	cli_app.cmdloop('''Welcome to the mkcluster interactive shell.
+	Type "new" to provision a new Couchbase cluster against a virgin instance.
+	Type "help" or "?" to list commands.''')
 
 
 if __name__ == '__main__':
---
+++ ansible/templates.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+
+couchbase_setup_ansible_command = '''
+---
+# 
+# Set up Couchbase cluster from a single command
+#
+     
+- name: Initialize the cluster and add the nodes to the cluster
+  hosts: couchbase-main
+  user: root 
+
+  tasks:
+  - name: Configure main node
+    shell: /opt/couchbase/bin/couchbase-cli cluster-init -c 127.0.0.1:8091  --cluster-init-username={{cluster_spec.admin_username}} --cluster-init-password={{cluster_spec.admin_password}} --cluster-init-port=8091 --cluster-init-ramsize={{cluster_spec.cluster_ram_quota}} 
+
+  - name: Create shell script for configuring main node
+    action: template src=couchbase-add-node.j2 dest=/tmp/addnodes.sh mode=750
+  
+  - name: Launch config script
+    action: shell /tmp/addnodes.sh
+  
+  - name: Rebalance the cluster
+    shell: /opt/couchbase/bin/couchbase-cli rebalance -c 127.0.0.1:8091 -u ${admin_user} -p ${admin_password}      
+
+{% for bucket in cluster_spec.buckets %}
+  - name: create bucket {{bucket.name}} with {{bucket.num_replicas}} replicas
+    shell: /opt/couchbase/bin/couchbase-cli bucket-create -c 127.0.0.1:8091 --bucket={{bucket.name}} --bucket-type={{bucket.type}} --bucket-port=11211 --bucket-ramsize={{bucket.ram_quota}}  --bucket-replica={{bucket.num_replicas}} -u {{cluster_spec.admin_user}} -p {{cluster_spec.admin_password}}
+{% endfor %}  
+
+'''
